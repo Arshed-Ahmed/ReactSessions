34d00560267d1b2b2b338d1ebba871de02ce1a17
Arshed <arshedahmed98@gmail.com>
2021-12-22 01:22:49 +0530

    Day 2

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './style.css';
+import Button from './Button/Button';
+import ButtonDesign from './ButtonDesign';
+
+function App() {
+  return (
+    <div>
+      <h1 style={{textAlign:"center"}}>Hello</h1>
+      <ButtonDesign />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ButtonDesign.js
@@ -0,0 +1,14 @@
+import './style.css';
+import Button from './Button/Button';
+
+function ButtonDesign() {
+  return (
+    <div>
+      <Button ButtonText="Apple" />
+      <Button ButtonText="Mango" />
+      <Button ButtonText="Guava" />
+    </div>
+  );
+}
+
+export default ButtonDesign;
